15dd78f27f0e1744e4d7f818dbf34628a1e21a29
Italo Castilho <italocosilva@gmail.com>
2023-08-25 07:17:56 -0300

    Lint src code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# digit-recognition
+![Banner](artifacts/demo.gif)
+
+# Handwriten Digit Recognition
+
+In this project I will show you how to implement a Convolutional Neural Network (CNN) usign one of the most traditional datasets for computer vision, the MNIST dataset. The objective here is to create a neural network capable of recognizing handwiten digits.
+
+After that I going to create a simple aplicattion where you can draw a digit and see if the neural network can predict the digit.
+
+## Licensing, Authors, Acknowledgements
+
+LeCun, Yann and Cortes, Corinna and Burges, CJ. MNIST handwritten digit database. ATT Labs [Online]. Available: http://yann.lecun.com/exdb/mnist. Volume 2, 2010.
--- src/main.py
+++ src/main.py
@@ -5,36 +5,42 @@ from predict import predict
 
 class DrawingApp:
     def __init__(self, root):
+        # Initialize tkinter root
         self.root = root
         self.root.title("Digit Recognition")
 
+        # Set initial result to 0
         self.result = 0
 
+        # Define canvas size and color
         self.canvas = tk.Canvas(self.root, width=320, height=320, bg="black")
         self.canvas.pack()
 
+        # Define PIL image to be equal to canvas
         self.drawing = Image.new("RGB", (320, 320), "black")
         self.draw2 = ImageDraw.Draw(self.drawing)
 
+        # Set actions of mouse buttons
         self.canvas.bind("<Button-1>", self.start_drawing)
         self.canvas.bind("<B1-Motion>", self.draw)
         self.canvas.bind("<ButtonRelease-1>", self.stop_drawing)
 
-        # Botão para realizar a ação
+        # Create button for prediction
         self.action_button = tk.Button(
             root, text="Predict", command=self.perform_action
         )
         self.action_button.pack()
 
-        # Botão para limpar
+        # Clear button
         self.clear_button = tk.Button(root, text="Clear", command=self.clear)
         self.clear_button.pack()
 
-        # Resultado da ação
+        # Label for predictin result
         self.action_result = tk.StringVar()
         self.result_label = tk.Label(root, textvariable=self.action_result)
         self.result_label.pack()
 
+        # Set initial state
         self.is_drawing = False
         self.prev_x = None
         self.prev_y = None
@@ -75,9 +81,9 @@ class DrawingApp:
         self.draw2 = ImageDraw.Draw(self.drawing)
 
 
-# Configuração da janela
+# Window configuration
 root = tk.Tk()
 app = DrawingApp(root)
 
-# Iniciar loop da interface
+# Initiate loop interface
 root.mainloop()
